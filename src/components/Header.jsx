1b9fcc110ece8a13ee49516997fc21932f1a033a
LoulouPlou <leia.plourde@hotmail.com>
2025-05-09 19:53:13 -0400

    ajustement header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ function Header() {
                     <div className="dropdown">
                         <Link className="dropbtn mainlinknav" to={`/Profil`}>Profil</Link>
                         <div className="dropdown-content">
-                            <Link className="" to={`/FavoriteRecipe`}> Favoris </Link>
+                            <Link className="" to={`/MyRecipes`}> Mes recettes </Link>
                             <Link className="" to={`/Connexion`}> Connexion </Link>
                             <Link className="" to={`/Inscription`}> Inscription </Link>
 
