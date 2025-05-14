ecdd93d13fd42544f2686c4c1e6bd41df351ef3b
LoulouPlou <leia.plourde@hotmail.com>
2025-05-13 20:34:30 -0400

    correction getItem si vide

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,9 +18,9 @@ function Header() {
     }
 
     useEffect(() => {
-        if(isConnected === true){
+        const id = localStorage.getItem('userId');
+        if(isConnected === true && id){
             setProfilSousMenu("Mon profil");
-            const id = localStorage.getItem('userId');
             setProfilMenuLien(`Profil/${id}`);
             setProfilSousMenu2Lien(`Profil/${id}`);
             setProfilSousMenu1Lien(`MyRecipes/${id}`);
